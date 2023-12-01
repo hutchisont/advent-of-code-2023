63146651ad1f00177dd1a59a3fefec1a4d500a2f
hutchisont <24536068+hutchisont@users.noreply.github.com>
2023-12-01 03:31:02 -0600

    day 1 part 1 done, still need to do part 2

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,4 +1,38 @@
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 
-fn main() {
-    
+fn main() -> io::Result<()> {
+    let f = File::open("input/day_1.txt")?;
+    let reader = BufReader::new(f);
+
+    let mut calibration_sum = 0;
+    for line in reader.lines() {
+        calibration_sum += extract_calibration_value(line?);
+    }
+    println!("{calibration_sum}");
+    Ok(())
 }
+
+fn extract_calibration_value(line: String) -> i32 {
+    let mut first_digit = 'a';
+    let mut second_digit = 'a';
+
+    for c in line.chars() {
+        if c.is_digit(10) {
+            first_digit = c;
+            break;
+        }
+    }
+    for c in line.chars().rev() {
+        if c.is_digit(10) {
+            second_digit = c;
+            break;
+        }
+    }
+
+    let combined_value = format!("{first_digit}{second_digit}");
+    combined_value.parse::<i32>().unwrap()
+}
+
