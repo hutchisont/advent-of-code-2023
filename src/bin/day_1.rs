49cbeb8ebacad6b9c5d281039e0c602a49b1d303
hutchisont <24536068+hutchisont@users.noreply.github.com>
2023-12-03 03:11:51 -0600

    update to work for day 2

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,17 +1,24 @@
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::fs::File;
 
 fn main() -> io::Result<()> {
     let f = File::open("input/day_1.txt")?;
     let reader = BufReader::new(f);
 
     let mut calibration_sum = 0;
+    let mut line_count = 0;
     for line in reader.lines() {
-        calibration_sum += extract_calibration_value(line?);
+        line_count += 1;
+        let line = line?;
+        println!("starting line: {line}");
+        let line = convert_words_to_digits(line);
+        println!("ending line: {line}");
+        calibration_sum += extract_calibration_value(line);
     }
-    println!("{calibration_sum}");
+    println!("line count: {line_count}");
+    println!("calibration sum: {calibration_sum}");
     Ok(())
 }
 
@@ -36,3 +43,34 @@ fn extract_calibration_value(line: String) -> i32 {
     combined_value.parse::<i32>().unwrap()
 }
 
+fn convert_words_to_digits(line: String) -> String {
+    let mut digitized_line = String::new();
+
+    for i in 0..line.len() {
+        let reduced_line = &line[i..];
+        let result = if reduced_line.starts_with("one") {
+            "1"
+        } else if reduced_line.starts_with("two") {
+            "2"
+        } else if reduced_line.starts_with("three") {
+            "3"
+        } else if reduced_line.starts_with("four") {
+            "4"
+        } else if reduced_line.starts_with("five") {
+            "5"
+        } else if reduced_line.starts_with("six") {
+            "6"
+        } else if reduced_line.starts_with("seven") {
+            "7"
+        } else if reduced_line.starts_with("eight") {
+            "8"
+        } else if reduced_line.starts_with("nine") {
+            "9"
+        } else {
+            &line[i..i + 1]
+        };
+        digitized_line += result;
+    }
+
+    digitized_line
+}
